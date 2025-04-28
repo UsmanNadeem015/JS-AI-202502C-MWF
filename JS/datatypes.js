ddaff08a016f80dd313d026f8a3352389e4e6b24
STUDENT <std1@LABSFC.COM>
2025-04-28 00:44:41 -0700

    lecture 2

---
+++ JS/datatypes.js
@@ -0,0 +1,62 @@
+console.log("Datatypes in JS");
+
+// let age = 22;
+
+// let name = "Usman";
+
+// let ispresent = true;
+
+// let anime = ["Bleach","JJK","Dragon ball Z"]
+
+// let usman = {
+//     name: "Usman Nadeem",
+//     cms : "3524-2023",
+//     age: 22,
+//     program: "BS DSWT"
+// }
+
+// function func_ex(){
+//     console.log("How to train your dragon")
+// }
+
+// let check_datatype = typeof(func_ex)
+
+// console.log(check_datatype)
+
+// console.log("Name(string):" + " " + name)
+
+// console.log("Age(integer):" + " " + age)
+
+// console.log("Anime(array):" + " " + anime)
+
+// console.log("Usman(object):" + " " + usman)
+
+// console.log("Movie:" + " " + func_ex())
+
+function plus(a,b){
+    console.log(a + b)
+}
+
+function minus(a,b){
+    console.log(a - b)
+}
+
+function divide(a,b){
+    console.log(a / b)
+}
+
+function multiply(a,b){
+    console.log(a * b)
+}
+
+console.log("Plus")
+plus(10,7)
+
+console.log("Minus")
+minus(10,7)
+
+console.log("Divide")
+divide(10,7)
+
+console.log("Multiply")
+multiply(10,7)
