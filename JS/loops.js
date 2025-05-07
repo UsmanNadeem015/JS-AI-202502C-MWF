e270b698baf0eab32730169a79e1b872445c8271
unknown <usmannadee015@gmail.com>
2025-05-07 00:47:37 -0700

    Any text here

---
+++ JS/loops.js
@@ -0,0 +1,34 @@
+console.log("Loops in JS");
+
+// For Loop 
+// for(let i=0; i<=10; i++){
+// console.log(i);
+// console.log("Hello world!");
+// }
+
+// While loop
+// let i = 0;
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+// Do While Loop
+// let i = 10;
+// do{
+//     console.log(i);
+//     i--
+// }
+// while(i>=1)
+
+
+
+
+
+
+// While loop
+// let i = 0;
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
---
+++ JS/tablejs.js
@@ -0,0 +1,20 @@
+let userInput = prompt('Enter a number:');
+let result = 0;
+document.write('Table for: ' + userInput + '<br>')
+
+// table using while 
+// let i = 1;
+// while(i <= 10){
+//     result = userInput * i
+//     document.write(userInput + ' x ' + i + ' = ' + result + '<br>')
+//     i++
+// }
+
+// table using do while 
+let i = 1;
+do{
+    result = userInput * i
+   document.write(userInput + ' x ' + i + ' = ' + result + '<br>')
+   i++
+}
+while(i <= 10)
