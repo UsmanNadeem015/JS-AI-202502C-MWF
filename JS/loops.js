6127bbaba9c9b7f5bee1cc5bbf6b5075ecf7a58e
unknown <usmannadee015@gmail.com>
2025-05-09 00:48:37 -0700

    more loops

--- JS/loops.js
+++ JS/loops.js
@@ -1,10 +1,10 @@
 console.log("Loops in JS");
 
-For Loop 
-for(let i=0; i<=10; i++){
-console.log(i);
-console.log("Hello world!");
-}
+// For Loop 
+// for(let i=0; i<=10; i++){
+// console.log(i);
+// console.log("Hello world!");
+// }
 
 // While loop
 // let i = 0;
@@ -22,13 +22,28 @@ console.log("Hello world!");
 // while(i>=1)
 
 
-
-
-
-
 // While loop
 // let i = 0;
 // while(i <= 10){
 //     console.log(i);
 //     i++
 // }
+
+// For in loop (Objects)
+let employee = {
+    id: 2,
+    Name: 'Usman Nadeem',
+    email: 'usmannadeem015@gmail.com'
+}
+
+for(let key in employee){
+console.log(employee[key]);
+}
+
+console.log('------------------------------------------------');
+
+// For of loop (array)
+let fruits =['apple','mangos','grapes']
+for(let basket of fruits){
+    console.log(basket);
+}
